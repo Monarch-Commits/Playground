f9608ffe025b1fedfdfe1e0695912b1e3b54c674
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-03-10 08:07:52 +0800

    a

--- app/components/Ai/AnimaBot.tsx
+++ app/components/Ai/AnimaBot.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Lottie from 'lottie-react';
-import animaBot from "@/public/animations/animabot.json";
+import animaBot from "@/app/public/animations/animabot.json";
 
 type AnimaBotProps = {
   width?: number;
