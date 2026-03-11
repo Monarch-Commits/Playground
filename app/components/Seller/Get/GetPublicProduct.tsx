b69d415f6009988205c92460fb8b151c4ab853d7
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-03-11 09:16:42 +0800

    a

--- app/components/Seller/Get/GetPublicProduct.tsx
+++ app/components/Seller/Get/GetPublicProduct.tsx
@@ -6,7 +6,7 @@ export default async function GetPublicProduct() {
   const data = await getPublicProduct();
   return (
     <>
-    <h1>Hello I am public</h1>
+    <h1>Hello I am public in test</h1>
     <div className="columns-1 sm:columns-2 lg:columns-3 gap-4 p-5 mt-10">
       {data.map((p) => (
         <GetChild key={p.id} p={p} />
