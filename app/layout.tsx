ec5b6ad739615f2b8d27bf1df96513c77aa50233
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-03-23 13:22:47 +0800

    aaaaaaaaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={` ${poppins.variable} antialiased`}>
           <Navigation />
-          <main className="pt-24">{children}</main>
+          {children}
           <Footer />
           <Toaster position="top-center" reverseOrder={false} />
         </body>
