30b242235db0958a61852b44c6f887ce047c8d72
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-03-26 09:15:24 +0800

    a

--- app/Shop/Child.tsx
+++ app/Shop/Child.tsx
@@ -16,7 +16,7 @@ export default async function Child({ category, page = 1 }: ChildProps) {
   return (
     <>
       {/* Product Grid */}
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+      <div className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {products.length > 0 ? (
           products.map((p) => (
             <div
--- app/Shop/page.tsx
+++ app/Shop/page.tsx
@@ -49,7 +49,7 @@ export default async function Page({
       <Suspense
         key={`${category}-${page}`}
         fallback={
-          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+          <div className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             <ShopSkeleton />
           </div>
         }
